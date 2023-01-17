c8933dd34ef4137624df0e668f86533ccab10a60
ignaciocervino <ignaciocervino@gmail.com>
2023-01-17 11:41:51 -0300

    first steps: ui babystep

---
+++ project_2/swiftui-project2/swiftui-project2/swiftui_project2App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_project2App.swift
+//  swiftui-project2
+//
+//  Created by Ignacio Cervino on 17/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_project2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
