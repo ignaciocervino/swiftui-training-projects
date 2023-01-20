2475b0945cfb45d02e2d980474847836be2262c0
ignaciocervino <ignaciocervino@gmail.com>
2023-01-20 16:35:34 -0300

    initial apporach

---
+++ project_3/ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Ignacio Cervino on 17/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
