5cc29336d480a531136a7e9ebe6b6085d8bae317
Ignacio Cervino <ignaciocervino@Ignacios-MacBook-Pro.local>
2023-07-26 23:20:44 -0300

    Init project challenge 19

---
+++ ChallengeDay19/ChallengeDay19/ChallengeDay19App.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeDay19App.swift
+//  ChallengeDay19
+//
+//  Created by Ignacio Cervino on 26/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeDay19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
